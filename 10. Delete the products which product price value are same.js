bbe49ddba5974f5a0815589e118ebb99e43b8f65
Navaneetha Krishnan <38004363+Nav-Krish@users.noreply.github.com>
2023-10-31 18:08:01 +0530

    Update 10. Delete the products which product price value are same.js

--- 10. Delete the products which product price value are same.js
+++ 10. Delete the products which product price value are same.js
@@ -1,5 +1,21 @@
 // 10. Delete the products which product price value are same
 
-db.products.deleteMany({
-    product_price: { $eq: db.products.distinct("product_price") },
+db.temp.aggregate([
+  {
+    $group: {
+      _id: "$product_price",
+      dups: {
+        $push: "$id",
+      },
+      count: {
+        $sum: 1,
+      },
+    },
+  },
+  {
+      $match:{count: {$gt:1}}
+  }
+]).forEach(function (doc) {
+    doc.dups.shift();
+    db.temp.remove({ _id: { $in: doc.dups } });
   });
